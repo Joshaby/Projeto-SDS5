0e3b6748273dacfb49c47e3c74a74da189438b6c
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2021-11-06 19:27:13 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,14 @@
+import { seller } from "./seller"
+
+export type Sale = {
+    id: number,
+    visited: number,
+    deals: number,
+    amount: number,
+    date: string;
+    seller: seller
+}
+
 export type SaleSum = {
     sellerName: string,
     sum: number
@@ -7,4 +18,16 @@ export type SaleSuccess = {
     sellerName: string,
     visited: number,
     deals: number
+}
+
+export type SalePage = {
+    content?: Sale[],
+    last: boolean,
+    totalElements: number,
+    totalPages: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number,
+    empty?: boolean
 }
